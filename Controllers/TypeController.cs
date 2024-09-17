d423e04639052dcad9cdcc244b5f8cc394809ed4
Afshin Azadegan <afshin.azadegon@yahoo.dk>
2024-09-17 11:45:28 +0200

    Please Work!!!!!!!!

---
+++ Controllers/TypeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToneForum.Repository.Interfaces;
+
+namespace ToneForum.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TypeController : ControllerBase
+    {
+        private readonly ITypeRepository repo;
+
+        public TypeController(ITypeRepository repo)
+        {
+            this.repo = repo;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
+using ToneForum.Repository.Interfaces;
 using ToneForum.Repository.Models;
+using ToneForum.Repository.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(
+            options => options.JsonSerializerOptions.ReferenceHandler
+            = ReferenceHandler.IgnoreCycles);
+
+// Repositories: 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICollectionListRepository, CollectionListRepository>();
+builder.Services.AddScoped<IWantListRepository, WantListRepository>();
+builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+builder.Services.AddScoped<IBandRepository, BandRepository>();
+builder.Services.AddScoped<IReleaseRepository, ReleaseRepository>();
+builder.Services.AddScoped<IFormatRepository, FormatRepository>();
+builder.Services.AddScoped<ITypeRepository, TypeRepository>();
+
+// Connection string: 
 builder.Services.AddDbContext<DataContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("ToneForumString")));
 var app = builder.Build();
